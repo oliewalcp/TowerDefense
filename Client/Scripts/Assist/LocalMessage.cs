83fa59c285b17ba5c1ba6c17e46e76ca7a9739c1
oliewalcp <811299838@qq.com>
2018-09-08 21:54:00 +0800

    Add files via upload

--- Client/Scripts/Assist/LocalMessage.cs
+++ Client/Scripts/Assist/LocalMessage.cs
@@ -4,6 +4,7 @@ public class LocalMessage {
 	public static ulong LocalPlayerNumber = 0;//本机玩家编号
 	public static ushort LocalRoomNumber = 0;//本机所在房间号
 	public static byte LocalIdentity = 1;//0房主，1普通玩家
+	public static byte[][] Map = null;//地图
 	public static SocketCom scom = new SocketCom();
 	public static Thread comThread = new Thread(Loop);
 
@@ -18,6 +19,23 @@ public class LocalMessage {
 		} else {
 			handler = h;
 			scom.SetMessageHandler(handler);
+		}
+	}
+	/* 根据数据生成地图
+	   param[arg]:地图的数据
+	   param[grid_bit]:一格地图占用的位数
+	*/
+	public static void SetMap(byte[] arg, int grid_bit = 2){
+		ushort lineNumber = arg[4], columnNumber = arg[5];
+		Map = new byte[lineNumber][];
+		int i, j;
+		for(i = 0; i < lineNumber; i++){
+			Map[i] = new byte[columnNumber];
+			ushort lastShift = 0;//上一次移位
+			byte temp = arg[i + 6];
+			for(j = 0; j + grid_bit < 8; j++){
+				//未完待续
+			}
 		}
 	}
 }
