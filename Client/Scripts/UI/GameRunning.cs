fbf2f7490f74f08a44f206086aa55becaf537194
oliewalcp <811299838@qq.com>
2018-09-13 10:22:28 +0800

    Add files via upload

--- Client/Scripts/UI/GameRunning.cs
+++ Client/Scripts/UI/GameRunning.cs
@@ -9,23 +9,40 @@ public class GameRunning : MonoBehaviour {
 	private string[] Floor = {"Prefabs/ground", "Prefabs/glass", "Prefabs/stone"};
 	private const float MapWidth = 668;
 	private const float MapHeight = 668;
-
 	void Start() {
-
 	}
 	// Use this for initialization
 	void OnEnable () {
-		LocalMessage.Map = new byte[10][];
-		for(int i = 0; i < 10; i++){
-			LocalMessage.Map[i] = new byte[10];
-		}
+		byte[] testMap = new byte[]{0, 0, 0, 0, 20, 20,
+								85, 85, 85, 85,  85,
+								85, 85, 85, 85,  69,
+								85, 85, 85, 85,  69,
+								64, 5,  0,  0,   69,
+								69, 69, 85, 21,  69,
+								69, 69, 85, 21,  69,
+								69, 69, 85, 21,  69,
+								69, 69, 85, 21,  69,
+								69, 69, 85, 21,  69,
+								69, 69, 85, 21,  69,
+								69, 69, 85, 21,  69,
+								5,  64, 85, 21,  69,
+								85, 85, 85, 21,  69,
+								85, 85, 85, 21,  69,
+								1,  0,  0,  0,   69,
+								81, 85, 85, 85,  69,
+								81, 85, 85, 85,  69,
+								1,  0,  0,  0,   64,
+								85, 85, 85, 85,  85,
+								85, 85, 85, 85,  85,
+								0, 0, 0, 0, 0, 0, 0, 0};
+		LocalMessage.SetMap(testMap);
+		//Debug.Log(testMap.Length);
 		LoadMap();
 	}
 	//加载地图
 	private void LoadMap(){
 		int line = LocalMessage.Map.Length, column = LocalMessage.Map[0].Length;
-		float width = MapWidth / column, height = MapHeight / line, halfWidth = width / 2, halfHeight = height / 2;
-
+		float width = MapWidth / column, height = MapHeight / line, halfWidth = width / 1, halfHeight = height / 1;
 		Vector3 scale = new Vector3(width, height, 0.01f);
 		for(int i = 0; i < line; i++){
 			for(int j = 0; j < column; j++){
