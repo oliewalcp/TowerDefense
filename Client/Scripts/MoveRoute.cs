3113c9f4376c9c729a12d45f104eadb5627467a2
oliewalcp <811299838@qq.com>
2018-09-14 08:59:41 +0800

    Add files via upload

---
+++ Client/Scripts/MoveRoute.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoute : MonoBehaviour {
+	public GameObject MapPanel;//地图区域
+	private int CurrentLine = 0;//物体当前所在的格子（行）
+	private int CurrentColumn = 0;//物体当前所在的格子（列）
+	// Use this for initialization
+	void Start () {
+		transform.SetParent(MapPanel.transform);
+		float small = LocalMessage.grid.height > LocalMessage.grid.width ? LocalMessage.grid.width : LocalMessage.grid.height;
+		Vector3 scale = new Vector3(small, small, small);
+		UIFunction.SetScale(gameObject, scale);
+		UIFunction.SetMapPosition(gameObject, LocalMessage.StartGrid);
+	}
+
+	void OnEnable() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDisable() {
+		Destroy(gameObject);
+	}
+}
