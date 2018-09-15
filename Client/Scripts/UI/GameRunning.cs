9828be24134ed415a5ca55e1fcd9eea256fe5789
oliewalcp <811299838@qq.com>
2018-09-15 16:16:41 +0800

    Add files via upload

--- Client/Scripts/UI/GameRunning.cs
+++ Client/Scripts/UI/GameRunning.cs
@@ -6,14 +6,16 @@ public class GameRunning : MonoBehaviour {
 
 	public GameObject MapPanel;//地图区域
 
-	private string[] Floor = {"Prefabs/ground", "Prefabs/glass", "Prefabs/stone"};
+	private readonly string[] Floor = {"Prefabs/ground", "Prefabs/glass", "Prefabs/stone"};
 	private const float MapWidth = 668;
 	private const float MapHeight = 668;
 	void Start() {
 	}
 	// Use this for initialization
 	void OnEnable () {
-		byte[] testMap = new byte[]{0, 0, 0, 0, 20, 20,
+		byte[] testMap = new byte[]{0, 0, 0, 0, 20, 20, 
+								3, 0, 1, 18,//地图路线的起点和终点
+								//地图开始
 								85, 85, 85, 85,  85,
 								85, 85, 85, 85,  69,
 								85, 85, 85, 85,  69,
@@ -34,24 +36,26 @@ public class GameRunning : MonoBehaviour {
 								1,  0,  0,  0,   64,
 								85, 85, 85, 85,  85,
 								85, 85, 85, 85,  85,
+								//地图结束
 								0, 0, 0, 0, 0, 0, 0, 0};
 		LocalMessage.SetMap(testMap);
-		//Debug.Log(testMap.Length);
 		LoadMap();
 	}
 	//加载地图
 	private void LoadMap(){
 		int line = LocalMessage.Map.Length, column = LocalMessage.Map[0].Length;
-		float width = MapWidth / column, height = MapHeight / line, halfWidth = width / 1, halfHeight = height / 1;
+		float width = MapWidth / column, height = MapHeight / line, halfWidth = width / 2, halfHeight = height / 2;
 		Vector3 scale = new Vector3(width, height, 0.01f);
 		for(int i = 0; i < line; i++){
 			for(int j = 0; j < column; j++){
 				GameObject go = (GameObject)Instantiate(Resources.Load(Floor[LocalMessage.Map[i][j]]));
 				go.transform.SetParent(MapPanel.transform);
 				UIFunction.SetScale(ref go, ref scale);
-				UIFunction.Set3DPosition(ref go, new Vector3(halfWidth + i * width, - (halfHeight + j * height), -0.09f));
+				UIFunction.Set3DPosition(ref go, new Vector3(halfHeight + j * height, - (halfWidth + i * width), -0.09f));
 			}
 		}
+		LocalMessage.grid.width = width;
+		LocalMessage.grid.height = height;
 	}
 	
 	// Update is called once per frame
@@ -60,11 +64,7 @@ public class GameRunning : MonoBehaviour {
 	}
 
 	void OnDisable(){
-		GameObject[] child = MapPanel.GetComponentsInChildren<GameObject>();
-		Debug.Log(child.Length);
-		foreach (GameObject go in child){
-			Destroy(go);
-		}
+		UIFunction.ClearChild(ref MapPanel);
 		LocalMessage.SetHandler(null);
 	}
 }
