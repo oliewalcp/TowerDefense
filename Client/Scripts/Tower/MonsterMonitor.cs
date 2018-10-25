8d0a80459318503d9df603e6f5a0c5780e6c95c5
oliewalcp <811299838@qq.com>
2018-10-25 23:32:21 +0800

    Add files via upload

--- Client/Scripts/Tower/MonsterMonitor.cs
+++ Client/Scripts/Tower/MonsterMonitor.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterMonitor : MonoBehaviour {
-
+	private HashSet<GameObject> InnerMonster = new HashSet<GameObject>();//在攻击范围内的怪物
+	private static readonly HashSet<string> AssistTower = new HashSet<string>();
+	private GameObject CurrentAttack = null;//当前正在攻击的怪物
 	// Use this for initialization
 	void Start () {
+		if(AssistTower.Count < 4) {
+			AssistTower.Add("DefenseDownTower");
+			AssistTower.Add("SpeedDownTower");
+			AssistTower.Add("AttackUpTower");
+			AssistTower.Add("SpeedUpTower");
+		}
 		Tower t = TowerData.AllTower[gameObject.name];
 		SphereCollider collider = GetComponent<SphereCollider>();
 		collider.radius = TowerData.UnitRadius * t.attack_range;
@@ -17,14 +25,7 @@ public class MonsterMonitor : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Tower t = TowerData.AllTower[gameObject.name];
-		FeatureMessage fm = new FeatureMessage();
-		fm.features = t.features.features;
-		fm.value = t.features.number;
-		fm.time = t.features.time;
-		fm.create = true;
-		other.gameObject.SendMessage("FeaturesWithoutHurt", fm);
-		//Debug.Log("Enter");
+		InnerMonster.Add(other.gameObject);
 	}
 	void OnTriggerExit(Collider other) {
 		Tower t = TowerData.AllTower[gameObject.name];
@@ -32,7 +33,11 @@ public class MonsterMonitor : MonoBehaviour {
 		fm.features = t.features.features;
 		fm.create = false;
 		other.gameObject.SendMessage("FeaturesWithoutHurt", fm);
-		//Debug.Log("Exit");
+		InnerMonster.Remove(other.gameObject);
+		//如果塔当前正在攻击该物体
+		if(other.gameObject == CurrentAttack) {
+			CurrentAttack = GetClosestMonster();
+		}
 	}
 
 	void OnTriggerStay(Collider other) {
@@ -43,5 +48,26 @@ public class MonsterMonitor : MonoBehaviour {
 		fm.time = t.features.time;
 		fm.create = true;
 		other.gameObject.SendMessage("FeaturesWithoutHurt", fm);
+		if(CurrentAttack != null) {
+			Shot();
+		}
+	}
+
+	GameObject GetClosestMonster() {
+		Vector3 tower_pos = transform.position;
+		GameObject closest = null;
+		float min = -1;
+		foreach(GameObject go in InnerMonster) {
+			float temp = UIFunction.GetDistance(go.transform.position, tower_pos);
+			if(min > temp || min == -1) {
+				min = temp;
+				closest = go;
+			}
+		}
+		return closest;
+	}
+
+	private void Shot() {
+
 	}
 }
